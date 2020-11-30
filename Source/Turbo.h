5c3104dd181cd0dd669716ffa594eae16880e22a
Zach Waugh <zwaugh@gmail.com>
2020-11-30 14:34:49 -0500

    Add project

---
+++ Source/Turbo.h
@@ -0,0 +1,18 @@
+//
+//  Turbo.h
+//  Turbo
+//
+//  Created by Zach Waugh on 11/30/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Turbo.
+FOUNDATION_EXPORT double TurboVersionNumber;
+
+//! Project version string for Turbo.
+FOUNDATION_EXPORT const unsigned char TurboVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Turbo/PublicHeader.h>
+
+
