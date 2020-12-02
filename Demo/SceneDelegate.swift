d13a63a106f7c8d2e518c68ce4ddded2abadc7ac
Zach Waugh <zwaugh@gmail.com>
2020-12-02 17:20:02 -0500

    Add working demo

--- Demo/SceneDelegate.swift
+++ Demo/SceneDelegate.swift
@@ -1,53 +1,53 @@
-//
-//  SceneDelegate.swift
-//  Demo
-//
-//  Created by Zach Waugh on 12/2/20.
-//
-
 import UIKit
 import Turbo
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
-    var window: UIWindow?
-
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
+struct Demos {
+    static let simple = URL(string: "https://turbo-native-simple-demo.glitch.me")!
 }
 
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    private var session: Session!
+    private lazy var navigationController = UINavigationController()
+    
+    override init() {
+        super.init()
+        setup()
+    }
+    
+    private func setup() {
+        session = Session()
+        session.delegate = self
+    }
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let viewController = VisitableViewController(url: Demos.simple)
+        navigationController.viewControllers = [viewController]
+        window?.rootViewController = navigationController
+        self.session.visit(viewController)
+    }
+}
+
+extension SceneDelegate: SessionDelegate {
+    func session(_ session: Session, didFailRequestForVisitable visitable: Visitable, error: Error) {
+        let alert = UIAlertController(title: "Visit failed!", message: error.localizedDescription, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        navigationController.present(alert, animated: true)
+    }
+    
+    func session(_ session: Session, didProposeVisitToURL url: URL, options: VisitOptions, properties: PathProperties) {
+        let viewController = VisitableViewController(url: url)
+        
+        if options.action == .replace {
+            navigationController.viewControllers = navigationController.viewControllers.dropLast() + [viewController]
+        } else {
+            navigationController.pushViewController(viewController, animated: true)
+        }
+        
+        session.visit(viewController)
+    }
+}
