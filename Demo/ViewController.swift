16a786aac998b4da2c651164072aac56091473a5
Zach Waugh <zwaugh@gmail.com>
2020-12-02 12:37:11 -0500

    Add empty Demo project

---
+++ Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Zach Waugh on 12/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
