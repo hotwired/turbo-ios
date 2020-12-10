6385f9952ccc3c7827eb215429463b431ad41eea
Zach Waugh <zwaugh@gmail.com>
2020-12-10 12:16:41 -0500

    Add minimum iOS version to Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Turbo",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         .library(
             name: "Turbo",
