956484e3524eaf203d12807313e7119427a5771d
Joe Masilotti <joe@masilotti.com>
2021-02-16 13:05:43 -0800

    Allow VisitProposal construction outside of Turbo (#23)

--- Source/Visit/VisitProposal.swift
+++ Source/Visit/VisitProposal.swift
@@ -5,7 +5,7 @@ public struct VisitProposal {
     public let options: VisitOptions
     public let properties: PathProperties
     
-    init(url: URL, options: VisitOptions, properties: PathProperties = [:]) {
+    public init(url: URL, options: VisitOptions, properties: PathProperties = [:]) {
         self.url = url
         self.options = options
         self.properties = properties
