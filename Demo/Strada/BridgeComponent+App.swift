a2c21b265ec82e5f217a02e26a553d90b9e12b13
Denis Švara <denis.svara@gmail.com>
2023-09-13 13:39:21 +0200

    Implement Menu component.

--- Demo/Strada/BridgeComponent+App.swift
+++ Demo/Strada/BridgeComponent+App.swift
@@ -4,7 +4,8 @@ import Strada
 extension BridgeComponent {
     static var allTypes: [BridgeComponent.Type] {
         [
-            FormComponent.self
+            FormComponent.self,
+            MenuComponent.self
         ]
     }
 }
---
+++ Demo/Strada/MenuComponent.swift
@@ -0,0 +1,78 @@
+import Foundation
+import Strada
+import UIKit
+
+final class MenuComponent: BridgeComponent {
+    override class var name: String { "menu" }
+    
+    override func onReceive(message: Message) {
+        guard let event = Event(rawValue: message.event) else {
+            return
+        }
+        
+        switch event {
+        case .display:
+            handleDisplayEvent(message: message)
+        }
+    }
+    
+    // MARK: Private
+    
+    private var viewController: UIViewController? {
+        delegate.destination as? UIViewController
+    }
+    
+    private func handleDisplayEvent(message: Message) {
+        guard let data: MessageData = message.data() else { return }
+        showAlertSheet(with: data.title, items: data.items)
+    }
+    
+    private func showAlertSheet(with title: String, items: [Item]) {
+        let alertController = UIAlertController(title: title,
+                                                message: nil,
+                                                preferredStyle: .actionSheet)
+        
+        for item in items {
+            let action = UIAlertAction(title: item.title, style: .default) {[weak self] _ in
+                self?.onItemSelected(item: item)
+            }
+            alertController.addAction(action)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(cancelAction)
+        
+        viewController?.present(alertController, animated: true)
+    }
+    
+    private func onItemSelected(item: Item) {
+        reply(to: Event.display.rawValue,
+              with: SelectionMessageData(selectedIndex: item.index))
+    }
+}
+
+// MARK: Events
+
+private extension MenuComponent {
+    enum Event: String {
+        case display
+    }
+}
+
+// MARK: Message data
+
+private extension MenuComponent {
+    struct MessageData: Decodable {
+        let title: String
+        let items: [Item]
+    }
+    
+    struct Item: Decodable {
+        let title: String
+        let index: Int
+    }
+    
+    struct SelectionMessageData: Encodable {
+        let selectedIndex:Int
+    }
+}
