514fe31ab8b3df7859596d6ac24305cd74aec31c
Denis Švara <denis.svara@gmail.com>
2023-09-13 11:36:11 +0200

    Enable Strada debug logging in debug builds.

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -1,11 +1,13 @@
 import UIKit
 import Turbo
+import Strada
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         #if DEBUG
         TurboLog.debugLoggingEnabled = true
+        Strada.config.debugLoggingEnabled = true
         #endif
         
         return true
