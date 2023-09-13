fa853d1c6d923377679b2330b42073ea1cdbeec6
Denis Švara <denis.svara@gmail.com>
2023-09-13 15:47:03 +0200

    Document `Form` component.

--- Demo/Strada/FormComponent.swift
+++ Demo/Strada/FormComponent.swift
@@ -2,6 +2,8 @@ import Foundation
 import Strada
 import UIKit
 
+/// Bridge component to display a submit button in the native toolbar,
+/// which will submit the form on the page when tapped.
 final class FormComponent: BridgeComponent {
     override class var name: String { "form" }
     
