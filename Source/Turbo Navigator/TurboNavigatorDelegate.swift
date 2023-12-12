d36ebf2502247d76aed85d2cc33a70ab711647cc
jpsphaxer <jpsphaxer93@gmail.com>
2023-12-12 13:58:31 -0500

    TurboNavigatorDelegate Docs

--- Source/Turbo Navigator/TurboNavigatorDelegate.swift
+++ Source/Turbo Navigator/TurboNavigatorDelegate.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+/// Contract for handling navigation requests and actions
+/// - Note: Methods  are __optional__ by default implementation in ``TurboNavigatorDelegate`` extension.
 public protocol TurboNavigatorDelegate: AnyObject {
     typealias RetryBlock = () -> Void
 
@@ -11,7 +13,6 @@ public protocol TurboNavigatorDelegate: AnyObject {
     ///
     /// - Parameter proposal: `VisitProposal` navigation destination
     /// - Returns:`ProposalResult` - how to react to the visit proposal
-    /// - Note: optional
     func handle(proposal: VisitProposal) -> ProposalResult
 
     func handle(externalURL: URL) -> ExternalURLNavigationAction
@@ -19,12 +20,10 @@ public protocol TurboNavigatorDelegate: AnyObject {
     /// An error occurred loading the request, present it to the user.
     /// Retry the request by executing the closure.
     /// - Important: If not implemented, will present the error's localized description and a Retry button.
-    /// - Note: optional
     func visitableDidFailRequest(_ visitable: Visitable, error: Error, retry: @escaping RetryBlock)
 
     /// Respond to authentication challenge presented by web servers behing basic auth.
     /// If not implemented, default handling will be performed.
-    /// - Note: optional
     func didReceiveAuthenticationChallenge(_ challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void)
 }
 
