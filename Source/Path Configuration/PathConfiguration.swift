b637c14a097fe0747da63774c96fb9d646126d26
Joe Masilotti <joe@masilotti.com>
2024-03-13 15:43:05 -0700

    Remove unused parameter documentation

--- Source/Path Configuration/PathConfiguration.swift
+++ Source/Path Configuration/PathConfiguration.swift
@@ -53,8 +53,6 @@ public final class PathConfiguration {
     }
 
     /// Returns a merged dictionary containing all the properties that match this URL.
-    /// - Parameters:
-    ///   - matchQuery: Enable to include query items when matching URLs.
     public func properties(for url: URL) -> PathProperties {
         if Turbo.config.pathConfiguration.matchQuery, let query = url.query {
             return properties(for: "\(url.path)?\(query)")
