8a83594b96be6b2b4b224398dc88ab533bf011c5
Fernando Olivares <fernando.olivares@me.com>
2024-04-10 11:47:05 -0600

    Add view controller as a part of VisitProposal

--- Source/Visit/VisitProposal.swift
+++ Source/Visit/VisitProposal.swift
@@ -1,13 +1,19 @@
 import Foundation
+import UIKit
 
 public struct VisitProposal {
     public let url: URL
     public let options: VisitOptions
     public let properties: PathProperties
+    public let viewController: UIViewController?
     
-    public init(url: URL, options: VisitOptions, properties: PathProperties = [:]) {
+    public init(url: URL,
+                options: VisitOptions,
+                properties: PathProperties = [:],
+                viewController: UIViewController? = nil) {
         self.url = url
         self.options = options
         self.properties = properties
+        self.viewController = viewController
     }
 }
