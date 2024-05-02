cba87761294efa20f1eb1dae141b1b0e9decb514
Alexey Solilin <solilin@jetrockets.com>
2024-05-02 18:26:59 +0200

    Fix actionSheet position for iPad (#194)
    * Fix actionSheet for iPad
    
    * Fix Meny example for iPads

--- Demo/Strada/MenuComponent.swift
+++ Demo/Strada/MenuComponent.swift
@@ -6,47 +6,58 @@ import UIKit
 /// which will send the selected index of the tapped menu item back to the web.
 final class MenuComponent: BridgeComponent {
     override class var name: String { "menu" }
-    
+
     override func onReceive(message: Message) {
         guard let event = Event(rawValue: message.event) else {
             return
         }
-        
+
         switch event {
         case .display:
             handleDisplayEvent(message: message)
         }
     }
-    
+
     // MARK: Private
-    
+
     private var viewController: UIViewController? {
         delegate.destination as? UIViewController
     }
-    
+
     private func handleDisplayEvent(message: Message) {
         guard let data: MessageData = message.data() else { return }
         showAlertSheet(with: data.title, items: data.items)
     }
-    
+
     private func showAlertSheet(with title: String, items: [Item]) {
         let alertController = UIAlertController(title: title,
                                                 message: nil,
                                                 preferredStyle: .actionSheet)
-        
+
         for item in items {
             let action = UIAlertAction(title: item.title, style: .default) {[weak self] _ in
                 self?.onItemSelected(item: item)
             }
             alertController.addAction(action)
         }
-        
+
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
         alertController.addAction(cancelAction)
-        
+
+        // Set popoverController for iPads
+        if let popoverController = alertController.popoverPresentationController {
+            if let barButtonItem = viewController?.navigationItem.rightBarButtonItem {
+                popoverController.barButtonItem = barButtonItem
+            } else {
+                popoverController.sourceView = viewController?.view
+                popoverController.sourceRect = viewController?.view.bounds ?? .zero
+                popoverController.permittedArrowDirections = []
+            }
+        }
+
         viewController?.present(alertController, animated: true)
     }
-    
+
     private func onItemSelected(item: Item) {
         reply(to: Event.display.rawValue,
               with: SelectionMessageData(selectedIndex: item.index))
@@ -68,12 +79,12 @@ private extension MenuComponent {
         let title: String
         let items: [Item]
     }
-    
+
     struct Item: Decodable {
         let title: String
         let index: Int
     }
-    
+
     struct SelectionMessageData: Encodable {
         let selectedIndex:Int
     }
