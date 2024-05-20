5f42ee44bdb89b9eacd6c0dc0fcc9924e8b8e905
Joe Masilotti <joe@masilotti.com>
2024-05-20 08:23:48 -0700

    Fix demo app

--- Demo/Navigator.swift
+++ Demo/Navigator.swift
@@ -4,7 +4,11 @@ import Turbo
 /// A bridge "back" to Turbo world from native.
 /// See `NumbersViewController` for an example of navigating from native to web.
 protocol Navigator: AnyObject {
-    func route(_: URL)
+    func route(_ url: URL)
 }
 
-extension TurboNavigator: Navigator {}
+extension TurboNavigator: Navigator {
+    func route(_ url: URL) {
+        route(url, options: VisitOptions(action: .advance), parameters: nil)
+    }
+}
